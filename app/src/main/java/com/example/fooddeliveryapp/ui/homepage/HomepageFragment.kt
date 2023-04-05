b9c0e6d3214cd5b125363175fdef04d76543d909
zehrakuru <74616481+zehrakuru@users.noreply.github.com>
2023-04-05 23:11:07 +0300

    Fifth Commit - bottom navigation added

---
+++ app/src/main/java/com/example/fooddeliveryapp/ui/homepage/HomepageFragment.kt
@@ -0,0 +1,19 @@
+package com.example.fooddeliveryapp.ui.homepage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fooddeliveryapp.R
+
+class HomepageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_homepage, container, false)
+    }
+}
