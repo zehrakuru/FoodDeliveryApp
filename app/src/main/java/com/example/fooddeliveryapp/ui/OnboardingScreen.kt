0c56f1a2807eb56a55a43e1d9561cc44c39badc3
zehrakuru <74616481+zehrakuru@users.noreply.github.com>
2023-04-05 10:44:36 +0300

    Second Commit

---
+++ app/src/main/java/com/example/fooddeliveryapp/ui/OnboardingScreen.kt
@@ -0,0 +1,12 @@
+package com.example.fooddeliveryapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fooddeliveryapp.R
+
+class OnboardingScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_screen)
+    }
+}
---
+++ app/src/main/java/com/example/fooddeliveryapp/ui/SliderFragment1.kt
@@ -0,0 +1,17 @@
+package com.example.fooddeliveryapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fooddeliveryapp.R
+
+class SliderFragment1 : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_slider1, container, false)
+    }
+}
