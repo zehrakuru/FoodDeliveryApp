46dbb10241812745a18bcc83c73539c08f66da73
zehrakuru <74616481+zehrakuru@users.noreply.github.com>
2023-04-13 15:55:26 +0300

    Animation is added to Offer Page

--- app/src/main/java/com/example/fooddeliveryapp/ui/offers/OfferAdapter.kt
+++ app/src/main/java/com/example/fooddeliveryapp/ui/offers/OfferAdapter.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fooddeliveryapp.R
+import com.google.android.material.animation.AnimationUtils
 
 class OfferAdapter(private val mContext: Context, private val offersList: List<Offers>) :
 RecyclerView.Adapter<OfferAdapter.CardDesignObjectsHolder>(){
@@ -16,11 +18,13 @@ RecyclerView.Adapter<OfferAdapter.CardDesignObjectsHolder>(){
         var imageViewOffer: ImageView
         var txtViewTitle: TextView
         var txtViewDesc: TextView
+        var cardView: CardView
 
         init {
             imageViewOffer = view.findViewById(R.id.imageViewOffer)
             txtViewTitle = view.findViewById(R.id.txtViewTitle)
             txtViewDesc = view.findViewById(R.id.txtViewDesc)
+            cardView = view.findViewById(R.id.cardView)
         }
     }
 
@@ -40,7 +44,7 @@ RecyclerView.Adapter<OfferAdapter.CardDesignObjectsHolder>(){
         holder.txtViewTitle.text = offer.offer_title
         holder.txtViewDesc.text = offer.offer_desc
         holder.imageViewOffer.setImageResource(mContext.resources.getIdentifier(offer.offer_image_name, "drawable", mContext.packageName))
-
+        holder.cardView.startAnimation(android.view.animation.AnimationUtils.loadAnimation(mContext, R.anim.anim1))
 
     }
 
--- app/src/main/java/com/example/fooddeliveryapp/ui/offers/OffersFragment.kt
+++ app/src/main/java/com/example/fooddeliveryapp/ui/offers/OffersFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.fooddeliveryapp.R
 import com.example.fooddeliveryapp.databinding.FragmentOffersBinding
 
 class OffersFragment : Fragment() {
@@ -25,12 +24,18 @@ class OffersFragment : Fragment() {
         val o2 = Offers(2, "payment", "Check This!", "Lorem ipsum")
         val o3 = Offers(3, "payment", "Check This Out!", "Lorem ipsum")
         val o4 = Offers(4, "payment", "Last", "Lorem ipsum")
+        val o5 = Offers(5, "payment", "Last", "Lorem ipsum")
+        val o6 = Offers(6, "payment", "Last", "Lorem ipsum")
+        val o7 = Offers(7, "payment", "Last", "Lorem ipsum")
 
         offersArrayList = ArrayList<Offers>()
         offersArrayList.add(o1)
         offersArrayList.add(o2)
         offersArrayList.add(o3)
         offersArrayList.add(o4)
+        offersArrayList.add(o5)
+        offersArrayList.add(o6)
+        offersArrayList.add(o7)
 
         adapter = OfferAdapter(requireContext(), offersArrayList)
 
